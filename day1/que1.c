3583c6fe912bd4891ee2a16db9c68cd268d24678
shashank2-8 <shashankpandey12976@gmail.com>
2026-06-08 20:23:05 +0530

    day-1 uploaded.

---
+++ day1/que1.c
@@ -0,0 +1,16 @@
+//program to calculate the sum of first N natural numbers 
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    printf("enter the number :");
+    scanf("%d" ,&n);
+    for(int i=1;i<=n;i++)
+    {
+        sum=sum+i;
+
+    }
+    printf("sum of %d natural numbers is %d",n,sum);
+    return 0;
+
+}
---
+++ day1/que3.c
@@ -0,0 +1,16 @@
+//write a program to find factorial of a given number
+#include <stdio.h>
+int main()
+{
+    int n,i;
+    int fact=1;
+    printf("Enter the number");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        fact =fact*i;
+    }
+    printf("Factorial of %d is %d",n,fact);
+    return 0;
+}
